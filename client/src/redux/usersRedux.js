c6b51f21b5c2de6e4b8cc323be8efa9fadf72ccb
DanielleWalker35 <yeller22@gmail.com>
2018-05-07 09:49:00 -0600

    created the store and a users reducer and set up the very basic App beginning, set it up with Routes because I figured we would want those later, and Provider so we can use the store

---
+++ client/src/redux/usersRedux.js
@@ -0,0 +1,16 @@
+
+const initialState = {
+    usersData: [],
+    loading: true,
+    errMsg: ""
+}
+
+this.state = this.initialState;
+
+const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+export default usersReducer
