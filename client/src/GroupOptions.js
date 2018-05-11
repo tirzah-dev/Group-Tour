f042553e7ee697312c9dbb07e189dfa9b10eefcf
Lelana <cfsfresno@gmail.com>
2018-05-11 15:15:36 -0600

    added additional page frameworks for reference

---
+++ client/src/GroupOptions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GroupOptions() {
+    return (
+        <div>
+            Im the page that displays the travelers current groups they are part
+            of, and will have the button with link to page that allows traveler to 
+            create new trip 
+        </div>
+    )
+}
+
+export default GroupOptions
